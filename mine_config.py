092f45f72e57345bc02eee5781c6841475996855
nicolas.a.welsh <usacys@local>
2022-02-09 08:50:30 -0500

    seperated dicts for more clarity

---
+++ mine_config.py
@@ -0,0 +1,32 @@
+from pynput.keyboard import Key
+
+
+# Time ranges for randomized execution time
+macro_sleep = [0, 0.1]
+key_sleep   = [0.01, 0.02]
+
+
+# Config dictionaries
+control_keys = {
+    'toggle': Key.f9,
+    'exit'  : Key.f10
+}
+
+
+# Control dictionaries
+flags = {
+    'running'  : False, 
+    'exit'     : False,
+    'first_run': True
+}
+
+cmd_text = {
+    'instructions': "Input delay between mining (in seconds): ",
+    'tutorial_1'  : "--- Type {} to  start/stop mining ---".format(control_keys['toggle']),
+    'tutorial_2'  : "---- Type {} to  end the script ----".format(control_keys['exit']),
+    'breaker'     : "=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=",
+    'started'     : "------------- Macro started -------------",
+    'paused'      : "------------- Macro  paused -------------",
+    'terminated'  : "---------- Program  terminated ----------",
+    'warning'     : "=~=~=~=~ Watch out for monsters! ~=~=~=~="
+}
