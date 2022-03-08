16544dea8021a152d1f8b92a424e72bf0fe7027b
nicolasawelsh <usacys@local>
2022-03-08 10:04:38 -0500

    gui testinjg

--- gui.py
+++ gui.py
@@ -18,47 +18,24 @@ def runCommand(cmd, timeout=None, window=None):
 
 
 # https://stackoverflow.com/questions/55910191/how-do-i-print-results-to-the-command-line-instead-of-a-popup-window-using-pysim
+
 def main():
-    sg.theme('DarkAmber')
-    layout1 = [
-        [sg.Text('Choose your deley between mining')],
-        [sg.Button(2.5), sg.Button(3.0), sg.Button(3.5), 
-         sg.Button(4.0), sg.Button(4.5), sg.Button(5.0)]
-    ]
-    layout2 = [
-        [sg.Text('Enter the command you wish to run')],
-        [sg.Input(key='_IN_', default_text='python3 discord_bot.py'), sg.Button('Run')],
-        [sg.Output(size=(60,15))]
-    ]
-    toggle_layout = [[sg.Button('Start'), sg.Button('Pause')]]
+    layout = [
+        [sg.Multiline(size=(110, 30), echo_stdout_stderr=True, reroute_stdout=True, autoscroll=True, background_color='black', text_color='white', key='-MLINE-')],
+        [sg.T('Promt> '), sg.Input(key='-IN-', focus=True, do_not_clear=False)],
+        [sg.Button('Run', bind_return_key=True), sg.Button('Exit')]]
 
-    tabgrp = [
-        [sg.TabGroup(
-            [
-                [sg.Tab('Config', layout1, 
-                    border_width=10,
-                    tooltip='Delay Configuration',
-                    element_justification='center'),
-                sg.Tab('Macro Logs', layout2,
-                    border_width=10,
-                    tooltip='Macro Logs',
-                    element_justification='center')
-                ]
-            ],
-            tab_location='centertop'),
-        toggle_layout]
-    ]
-
-    window = sg.Window('Auto-Miner', tabgrp, 
-                    element_justification='c', resizable=True, finalize=True)
-
-    while True:
-        event, values = window.Read()
-        print(event, values)
-        if event in (None, 'Exit'):
+    window = sg.Window('Realtime Shell Command Output', layout)
+    while True:  # Event Loop
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit'):
             break
         elif event == 'Run':
-            runCommand(cmd=values['_IN_'], window=window)
-    window.Close()
+            sp = sg.execute_command_subprocess(values['-IN-'], pipe_output=True, wait=False)
+            results = sg.execute_get_results(sp)
+            print(results[0])
+
+    window.close()
+
 
 main()
