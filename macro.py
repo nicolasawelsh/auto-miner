68e4464573e3d1fb5d5c3fbe9e124ad2ece6bfda
nicolasawelsh <usacys@local>
2022-03-09 14:50:45 -0500

    implements new alert system

--- macro.py
+++ macro.py
@@ -5,12 +5,12 @@ from datetime        import datetime
 from time            import sleep, time
 from random          import uniform
 from os.path         import exists
+import pickle
 
 # Local libraries
 from config.config   import control_keys, flags, cmd_text, \
-                            execution_sleep, alerts
-from discord_bot     import detection_file
-
+                            execution_sleep
+from config.pickle_db import read_db, build_db
 
 def on_press(key):
     if key == control_keys['toggle']:
@@ -40,43 +40,30 @@ def toggle(toggle=True, pause=False):
 def mine_macro(flags):
     keyboard = Controller()
     while not flags['exit']:
-        
-        # Catch script time that can be subtracted from delay
         overhead_start_time = time()
-        # Wait some time to detect messages
-        sleep(2)
+        sleep(1.5)  # Wait for messages
+        try:
+            db = read_db()
+        except Exception as e:
+            build_db()
+            db = read_db()
 
-        # Read possible alert from discord_bot.py
-        detection_contents = read_file(detection_file)
-        
-        # Alert triggered
-        if detection_contents:
+        if db['monster_appeared']:
+            toggle(toggle=False, pause=True)
+            print_text('monster')
+            press_keys(keyboard, 'm!fight ')
+            while db['monster_appeared']:
+                db = read_db()
+            toggle(toggle=False, pause=False)
+            print_text('defeat')
+            rand_sleep('macro', do_sleep=True)
+        elif db['repair_needed']:
+            press_keys(keyboard, 'm!repair')
+            press_keys(keyboard, [Key.enter])
+            print_text('repair')
+            rand_sleep('macro', do_sleep=True)
 
-            # Monster appeared
-            if alerts['monster'] in detection_contents:
-                clear_file(detection_file)
-                toggle(toggle=False, pause=True)
-                print_text('monster')
-                press_keys(keyboard, 'm!fight ')
-                
-                # Wait for monster defeat
-                while alerts['defeat'] not in detection_contents:
-                    detection_contents = read_file(detection_file)
-
-                # Monster defeated
-                clear_file(detection_file)
-                print_text('defeat')
-                toggle(toggle=False, pause=False)
-                rand_sleep('macro', do_sleep=True)
-
-            # Repair needed
-            elif alerts['repair'] in detection_contents:
-                clear_file(detection_file)
-                press_keys(keyboard, 'm!repair')
-                press_keys(keyboard, [Key.enter])
-                print_text('repair')
-                rand_sleep('macro', do_sleep=True)
-
+        sleep(0.5)  # Wait for messages
         overhead_time = time() - overhead_start_time
         
         # Macro loop delay
@@ -112,25 +99,9 @@ def check_exit(s, flags):
 def rand_sleep(sleep_key, do_sleep=True):
     sleep_arr = execution_sleep[sleep_key]
     sleep_time = uniform(sleep_arr[0], sleep_arr[-1])
-    #sleep_time = 0
     if do_sleep:
         sleep(sleep_time)
     return sleep_time
-
-
-def clear_file(filename):
-    if exists(filename):
-        with open(filename, 'w') as fp:
-            fp.truncate(0)
-
-
-def read_file(filename):
-    if exists(filename):
-        with open(detection_file, 'r') as fp:
-            contents = fp.read()
-    else:
-        return False
-    return contents
 
 
 def print_text(argument):
